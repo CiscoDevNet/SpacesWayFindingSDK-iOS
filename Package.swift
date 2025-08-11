6b5643941c6fcc1168392c767f73457178e30430
bhariswa <bhariswa@cisco.com>
2025-08-11 19:53:30 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SpacesWayFindingApp",
+    platforms: [
+        .iOS(.v11) // Adjust based on your minimum iOS version
+    ],
+    products: [
+        .library(
+            name: "SpacesWayFindingApp",
+            targets: [
+                "Mapbox",
+                "SpacesWayFindingAppBinary",
+                "SpacesWayFindingSDK"
+            ]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Mapbox",
+            path: "Frameworks/Mapbox.xcframework"
+        ),
+        .binaryTarget(
+            name: "SpacesWayFindingAppBinary",
+            path: "Frameworks/SpacesWayFindingApp.xcframework"
+        ),
+        .binaryTarget(
+            name: "SpacesWayFindingSDK",
+            path: "Frameworks/SpacesWayFindingSDK.xcframework"
+        )
+    ]
+)
