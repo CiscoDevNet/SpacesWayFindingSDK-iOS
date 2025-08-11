80936655a1127b7f986e82f942cb2d603fa8cd9f
bhariswa <bhariswa@cisco.com>
2025-08-11 21:50:37 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,32 +2,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "SpacesWayFindingSDK-iOS",
+    name: "SpacesWayFindingSDK-iOS", // Your app package name (same as SDK repo/package)
     platforms: [
-        .iOS(.v16) // Adjust based on your minimum iOS version
+        .iOS(.v16)
     ],
-    products: [
-        .library(
-            name: "SpacesWayFindingSDK-iOS",
-            targets: [
-                "Mapbox",
-                "SpacesWayFindingApp",
-                "SpacesWayFindingSDK"
-            ]
+    dependencies: [
+        .package(
+            url: "https://github.com/CiscoDevNet/SpacesWayFindingSDK-iOS.git",
+            from: "1.0.0" // or your version tag/branch
         )
     ],
     targets: [
-        .binaryTarget(
-            name: "Mapbox",
-            path: "Frameworks/Mapbox.xcframework"
-        ),
-        .binaryTarget(
-            name: "SpacesWayFindingApp",
-            path: "Frameworks/SpacesWayFindingApp.xcframework"
-        ),
-        .binaryTarget(
-            name: "SpacesWayFindingSDK",
-            path: "Frameworks/SpacesWayFindingSDK.xcframework"
+        .target(
+            name: "SpacesWayFindingSDK-iOS", // Your app target name
+            dependencies: [
+                .product(name: "SpacesWayFindingSDK-iOS", package: "SpacesWayFindingSDK-iOS")
+            ]
         )
     ]
 )
